892e6c505b8342661bbac9d52956edf311af1099
Franklin Ebuka Ekwugha <fekwugha@axxess.com>
2023-05-04 15:43:15 +0100

    Add project files.

---
+++ ConsoleApp1/MassTransitEmailDto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MassTransitEmailDto
+    {
+
+        public string messageId { get; set; }
+        public string conversationId { get; set; }
+        public string sourceAddress { get; set; }
+        public string destinationAddress { get; set; }
+        public string[] messageType { get; set; }
+        public EmailDto message { get; set; }
+        public DateTime sentTime { get; set; }
+        public Headers headers { get; set; }
+        public Host host { get; set; }
+
+        public class Headers
+        {
+            public string MTActivityId { get; set; }
+        }
+
+        public class Host
+        {
+            public string machineName { get; set; }
+            public string processName { get; set; }
+            public int processId { get; set; }
+            public string assembly { get; set; }
+            public string assemblyVersion { get; set; }
+            public string frameworkVersion { get; set; }
+            public string massTransitVersion { get; set; }
+            public string operatingSystemVersion { get; set; }
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            string ServiceBusConnectionString = "";
+
+            try
+            {
+                var productRating = new MassTransitEmailDto
+                {
+                    message = new EmailDto
+                    {
+                        Receiver = "ekwugha.franklin@gmail.com",
+                        Body = "testing",
+                        Subject = "testing",
+
+                    }
+                };
+                for (int i = 0; i < 8; i++)
+                {
+                    var message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(productRating)));
+                    var topicClient = new TopicClient(ServiceBusConnectionString, "");
+                    await topicClient.SendAsync(message);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
